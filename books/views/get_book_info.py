144033c1e86398e64b4617950cbb15c733f7ebe6
eliseius <eliseevsi@gmail.com>
2023-10-17 23:48:17 +0300

    Done

--- books/views/get_book_info.py
+++ books/views/get_book_info.py
@@ -8,5 +8,5 @@ def get_book_info_view(request: HttpRequest, book_id: int) -> HttpResponse:
     if book_id > 0:
         book = Book.objects.filter(pk=book_id).first()
         if book is not None:
-            return render (request, 'book_info.html', context={"book": book})
+            return render(request, 'book_info.html', context={"book": book})
     return HttpResponse(status=404)
